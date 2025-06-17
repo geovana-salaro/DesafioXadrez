6768d7f896ede4983be7c507e1a1f56546252644
geovana-salaro <202502005695@alunos.estacio.br>
2025-06-16 22:12:56 -0300

    Update xadrez.c
    Estrutura

--- xadrez.c
+++ xadrez.c
@@ -4,4 +4,27 @@ int main(void){
 
     printf("Desafio Jogo de Xadrez\n");
 
+    // Movimento da torre com for
+    int casas_torre = 5;
+    for (int i = 0; i < casas_torre; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimento do bispo com while
+    int casas_bispo = 5;
+    int j = 0;
+    while (j < casas_bispo) {
+        printf("Cima, Direita\n");
+        j++;
+    }
+
+    // Movimento da rainha com do-while
+    int casas_rainha = 8;
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < casas_rainha);
+
+    return 0;
 }
